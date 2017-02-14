e2de8e2d627eecb8f0f5d9c0252913e0f735d28d
Michaël Opdebeeck <michael@factionxyz.com>
2017-02-14 19:57:33 +0100

    remove console.log

--- server/controllers/topic.js
+++ server/controllers/topic.js
@@ -3,9 +3,7 @@ import * as topicService from '../services/topic.crud';
 export async function create(req, res) {
   try {
     const { title } = req.body;
-    console.log('======= title', title);
     const topic = await topicService.create(title);
-    console.log('===== topic', topic);
     return res.json(topic);
   } catch (err) {
     console.log('Controllers::Topic:create::ERROR::', err);
